0aafcee3be80fb8b4b8a6361b3b54b910b9e4e71
DhanterDragneel <dekapramesta77@gmail.com>
2022-03-04 21:54:47 +0700

    try port

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require("express");
 const qrcode = require("qrcode");
 const http = require("http");
 const { response } = require("express");
+const port = process.env.PORT || 5000;
 
 const app = express();
 const server = http.createServer(app);
@@ -81,6 +82,6 @@ app.post("/send-message", (req, res) => {
     });
 });
 
-server.listen(8000, function () {
+server.listen(port, function () {
   console.log("app runing ");
 });
