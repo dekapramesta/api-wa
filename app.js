eb82621f6fadd625436990ef207caf4755ce1c5a
DhanterDragneel <dekapramesta77@gmail.com>
2022-03-04 22:52:17 +0700

    oi

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ client.on("authenticated", (session) => {
 });
 
 client.on("ready", () => {
-  console.log("Client i ready!");
+  console.log("Client is ready!");
 });
 
 client.on("message", (msg) => {
